ecf31748d93c97e46b434725e3b5ded8aa805f27
Amy Arora <amyarora2214@gmail.cpm>
2024-12-15 18:28:03 +0530

    A2SL & finalizing

---
+++ A2SL/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/',views.login_view,name='login'),
+    path('logout/',views.logout_view,name='logout'),
+    path('signup/',views.signup_view,name='signup'),
+    path('animation/',views.animation_view,name='animation'),
+    path('',views.home_view,name='home'),
+    path('animation/',views.animation_view,name='animation')
+]
